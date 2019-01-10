47bc1a5633c608b84f2c268ff255db779ee01aa1
AlexBharucha <>
2019-01-10 13:06:23 -0600

    Chapter 01 - Chapter 04

---
+++ src/Chapter01/P1.java
@@ -0,0 +1,18 @@
+package Chapter01;
+
+/**
+ * Program to say "Welcome to Java" once
+ *
+ * @author Alex Bharucha
+ */
+public class P1 {
+
+    /**
+     * Main Method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+}
